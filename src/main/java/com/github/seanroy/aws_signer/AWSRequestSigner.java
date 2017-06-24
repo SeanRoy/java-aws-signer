6183911ba7cc98b431550175417e0564baa317f7
Sean Roy <sean.roy@ping4.com>
2017-06-23 20:17:36 -0400

    First commit

---
+++ src/main/java/com/github/seanroy/aws_signer/AWSRequestSigner.java
@@ -0,0 +1,14 @@
+package com.github.seanroy.aws_signer;
+
+/**
+ * Hello world!
+ *
+ */
+public class AWSRequestSigner 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/github/seanroy/aws_signer/TaskOne.java
@@ -0,0 +1,143 @@
+package com.github.seanroy.aws_signer;
+
+import java.math.BigInteger;
+import java.net.URI;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class TaskOne {
+    public static final String HashAlgorithm = "SHA-256";
+    private URI uri;
+    private String HTTPRequestMethod;
+    private String CanonicalURI;
+    private String CanonicalQueryString;
+    private String CanonicalHeaders;
+    private String SignedHeaders;
+    private String RequestPayload;
+    
+    
+    public String getHTTPRequestMethod() {
+        return HTTPRequestMethod;
+    }
+
+    public void setHTTPRequestMethod(String hTTPRequestMethod) {
+        HTTPRequestMethod = hTTPRequestMethod;
+    }
+    
+    public TaskOne withHTTPRequestMethod(String hTTPRequestMethod) {
+        setHTTPRequestMethod(hTTPRequestMethod);
+        return this;
+    }  
+    
+    public void setURL(String url) {
+        try {
+            uri = new URI(url);
+            CanonicalURI = uri.getPath();
+            
+            String queryString = uri.getQuery();
+            
+            Map<String,String> queryPieces = new HashMap<String,String>();
+            
+            Stream.of(queryString.split("&")).forEach(seg -> {
+                String [] keyAndValue = seg.split("=");
+                queryPieces.put(keyAndValue[0], keyAndValue.length == 2 ? keyAndValue[1] : (""));
+            });
+            
+            CanonicalQueryString = queryPieces.keySet().stream().sorted().map(x -> {
+                return String.format("%s=%s",x, queryPieces.get(x));
+            }).collect(Collectors.joining("&"));           
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public TaskOne withURL(String url) {
+        setURL(url);  
+        return this;
+    }
+    
+    public void setHeaders(String [] headers) {
+        Map<String,String> kAndV = new HashMap<String,String>();
+        kAndV.put("host", uri.getHost().trim().toLowerCase());
+        
+        Stream.of(headers).forEach(header -> {
+            String [] pieces = header.trim().replaceAll("\\s+", " ").split(":");
+            kAndV.put(pieces[0].trim().toLowerCase(), pieces[1].trim());
+        });
+        
+        CanonicalHeaders = kAndV.keySet().stream().sorted().map(key -> {
+           return String.format("%s:%s", key, kAndV.get(key)); 
+        }).reduce("", (a,b) -> a + b + "\n");
+        
+        SignedHeaders = kAndV.keySet().stream().sorted().collect(Collectors.joining(";")) + "\n";
+        System.out.println(SignedHeaders);
+    }
+    public TaskOne withHeaders(String [] headers) {
+        setHeaders(headers);
+        return this;
+    }
+
+    public void setRequestPayload(String requestPayload) {
+        RequestPayload = requestPayload;
+    }
+    
+    public TaskOne withRequestPayload(String requestPayload) {
+        setRequestPayload(requestPayload);
+        return this;
+    }
+
+    public static byte [] hash(String payload) {
+        try {
+            return MessageDigest.getInstance(HashAlgorithm).digest(payload.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return payload.getBytes();
+    }
+    
+    public static String toHex(byte [] bytes) {
+        try {
+            return String.format("%040x", new BigInteger(1, bytes));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return new String(bytes);
+    }
+    
+    public String getCanonicalRequest() {
+        return
+                HTTPRequestMethod + '\n' +
+                CanonicalURI + '\n' +
+                CanonicalQueryString + '\n' +
+                CanonicalHeaders + '\n' +
+                SignedHeaders +
+                toHex(hash(RequestPayload));
+    }
+    
+    public String getHashedCanonicalRequest() {
+        return toHex(hash(getCanonicalRequest())).toLowerCase();
+    }
+    
+    public static void main(String [] args) {
+        String url = "https://iam.amazonaws.com/?Action=ListUsers&Version=2010-05-08";
+        String [] headers = {
+                "Content-Type: application/x-www-form-urlencoded; charset=utf-8", 
+                "X-Amz-Date: 20150830T123600Z"};
+        
+        TaskOne t = new TaskOne()
+            .withHTTPRequestMethod("GET")
+            .withURL(url)
+            .withRequestPayload("")
+            .withHeaders(headers);
+        
+        System.out.println(t.getCanonicalRequest());
+        System.out.println(t.getHashedCanonicalRequest());
+    }
+}
